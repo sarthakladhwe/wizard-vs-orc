47afbe032a48f6f342fc76deed227cfd3a530c0f
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-06-08 20:20:49 +0530

    Added datay.js file to store constructor function

--- script.js
+++ script.js
@@ -1,55 +1,41 @@
-function getDiceRollArray(diceCount) { 
+import characterData from './data.js'
+
+function getDiceRollArray(diceCount) {  
     return new Array(diceCount).fill(0).map(function(){
     return Math.floor(Math.random() * 6) + 1
     });   
 }
 
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function(num){ 
-        return  `<div class="dice">${num}</div>`
-    }).join('')
-}
-
-const hero = {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceCount: 3
-}
-
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceCount: 1
-}
-
 function Character(data) {
-    
-    const { elementId, name, avatar, health, diceCount } = data;
-    
     Object.assign(this, data)
+    
+    this.getDiceHtml = function(diceCount) {
+        return getDiceRollArray(diceCount).map(function(num){ 
+            return  `<div class="dice">${num}</div>`
+        }).join('')
+    }
 
-    this.getCharacterHTML = function() {
-        const diceHtml = getDiceHtml(this.diceCount)
-
-        document.getElementById(this.elementId).innerHTML =
-            `<div class="character-card">
-                <h4 class="name"> ${this.name} </h4>
-                <img class="avatar" src="${this.avatar}" />
-                <div class="health">health: <b> ${this.health} </b></div>
-                <div class="dice-container">    
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this;      
+        let diceHtml = this.getDiceHtml(diceCount);
+        
+           return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">
                     ${diceHtml}
                 </div>
             </div>`;
-    }
-
+    }  
 }
 
-const wizard = new Character(hero)
-const orc = new Character(monster)
+function render() {
+    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
+    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+}
 
-wizard.getCharacterHTML()
-orc.getCharacterHTML()
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+render()
