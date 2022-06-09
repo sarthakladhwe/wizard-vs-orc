82bde2eb33b6288f8e18913352ec765bfac1608d
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-06-09 19:41:40 +0530

    Created utils.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 import characterData from './data.js'
-
-function getDiceRollArray(diceCount) {  
-    return new Array(diceCount).fill(0).map(function(){
-    return Math.floor(Math.random() * 6) + 1
-    });   
-}
+import {getDiceRollArray} from './utils.js'
 
 function Character(data) {
     Object.assign(this, data)
